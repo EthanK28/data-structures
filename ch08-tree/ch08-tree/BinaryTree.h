8ba14be236238b74618ffa5e1c1366cd70404bdf
EthanK28 <kes0228@gmail.com>
2016-02-13 18:27:14 +0900

    Init

---
+++ ch08-tree/ch08-tree/BinaryTree.h
@@ -0,0 +1,25 @@
+//#ifndef __BINRARY_TREE_H__
+//#define __BINRARY_TREE_H__
+//
+//typedef int BTData;
+//
+//typedef struct _bTreeNode
+//{
+//	BTData data;
+//	struct _bTreeNode * left;
+//	struct _bTreeNode * right;
+//} BTreeNode;
+//
+//BTreeNode * MakeBTreeNode(void);
+//
+//BTData GetData(BTreeNode * bt);
+//void SetData(BTreeNode * bt, BTData data);
+//BTreeNode * GetLeftSubTree(BTreeNode * bt);
+//BTreeNode * GetRightSubTree(BTreeNode * bt);
+//
+//void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+//void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+//
+//
+//
+//#endif // !__BINRARY_TREE_H__
---
+++ ch08-tree/ch08-tree/BinaryTree2.h
@@ -0,0 +1,31 @@
+#ifndef __BINRARY_TREE_H__
+#define __BINRARY_TREE_H__
+
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+	BTData data;
+	struct _bTreeNode * left;
+	struct _bTreeNode * right;
+} BTreeNode;
+
+BTreeNode * MakeBTreeNode(void);
+
+BTData GetData(BTreeNode * bt);
+void SetData(BTreeNode * bt, BTData data);
+BTreeNode * GetLeftSubTree(BTreeNode * bt);
+BTreeNode * GetRightSubTree(BTreeNode * bt);
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
+
+typedef void VisitFuncPtr(BTData data);
+
+void PreorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void InorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+void PostorderTraverse(BTreeNode * bt, VisitFuncPtr action);
+
+
+
+#endif // !__BINRARY_TREE_H__
